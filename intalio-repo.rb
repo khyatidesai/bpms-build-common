b418eaab9dfd663441512ea85ceaed797ab24c7d
Khyati Desai <khyati@intalio.com>
2013-03-20 11:49:53 +0530

    Added a new intalio repository

--- intalio-repo.rb
+++ intalio-repo.rb
@@ -1,2 +1,3 @@
 repositories.remote << "http://release.intalio.com/m2repo/"
+repositories.remote << "http://10.10.2.106:8081/nexus/content/repositories/intalio/"
 repositories.remote << "http://release.intalio.com:9082/nexus/content/repositories/m2repo/"
